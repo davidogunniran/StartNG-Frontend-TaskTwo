e8cc5ed8cb147d267f7f25fe41401d8a9842c485
David <ayodeleogunniran@gmail.com>
2020-03-23 02:39:28 +0100

    js task- front end

---
+++ script.js
@@ -0,0 +1,12 @@
+//1. Store your name in a variable
+var myName = 'David Ogunniran';
+var courses = ['html', 'css', 'javascript'];
+
+console.log(myName);
+// displays David Ogunniran
+console.log(courses);
+// displays indexed list of courses
+
+for (let i=1; i<=200; i++) {
+    if(i%2 !== 0) console.log(i);
+} // diplays all odd numbers from 1 - 200 (inclusive)
